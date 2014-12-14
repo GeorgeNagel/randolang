f6d1d90b82666a96df67d52d6fbf56945d3ae7a2
gsusmobile <gsnagel@gmail.com>
2014-12-14 15:00:33 -0500

    Spelling is great. Need to filter out english words.

--- generate_words.py
+++ generate_words.py
@@ -1,26 +1,33 @@
 from randolang import (
-    generate_word, generate_markov_tree, entries_from_cmudict,
+    generate_new_sequence, generate_markov_tree, entries_from_cmudict,
     entries_from_mhyph
 )
+from phones_to_word import phones_to_word
 
 WORDS_TO_GENERATE = 1000
 MAX_TRANSITIONS = 100
-ORDER = 3
-method = 'cmu'
+ORDER = 1
+method = 'mhyph'
 
 if method == 'cmu':
     cmu_entries = entries_from_cmudict(filt='Austen')
     print "Generating transitions dict."
     sequences = [phones for word,phones in cmu_entries]
-    transitions_dict = generate_markov_tree(sequences, order=ORDER)
+    markov_tree = generate_markov_tree(sequences, order=ORDER)
     print "Done generating transitions dict."
 elif method == 'mhyph':
     mhyph_entries = entries_from_mhyph(filt='Austen')
+    markov_tree = generate_markov_tree(mhyph_entries, order=ORDER)
 
-words = []
+new_sequences = []
 for i in range(WORDS_TO_GENERATE):
-    word = generate_word(transitions_dict, MAX_TRANSITIONS)
-    words.append(word)
+    new_sequence = generate_new_sequence(markov_tree, MAX_TRANSITIONS)
+    new_sequences.append(new_sequence)
+
+if method == 'cmu':
+    words = [phones_to_word(phones) for phones in new_sequences]
+elif method == 'mhyph':
+    words = [''.join(syllables) for syllables in new_sequences]
 
 story = ' '.join(words)
 print "%s" % story
