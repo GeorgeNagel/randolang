c1d5821b1480c8eee1bdf5407a4efd750aeaa6e5
gsusmobile <gsnagel@gmail.com>
2014-12-14 22:03:14 -0500

    Updating whois to use new refactored code. Still needs more tests.

--- whois.py
+++ whois.py
@@ -4,37 +4,19 @@ from time import sleep
 
 import pythonwhois
 
-from randolang import entries_from_cmudict, entries_from_mhyph
-from markov import generate_new_sequence, generate_markov_tree
-from phones_to_word import phones_to_word
-
-PHONES_METHOD = 'phones_method'
-SYLLABLES_METHOD = 'syllables_method'
+from randolang import generate_words
 
 DOMAINS_TO_GENERATE = 100
-MAX_SEQUENCE_SIZE = 100
-ORDER = 1
-
-method = SYLLABLES_METHOD
-
-print "Generating Markov tree."
-if method == PHONES_METHOD:
-    cmu_entries = entries_from_cmudict(filt='Austen')
-    sequences = [phones for word, phones in cmu_entries]
-    markov_tree = generate_markov_tree(sequences, order=ORDER)
-elif method == SYLLABLES_METHOD:
-    mhyph_entries = entries_from_mhyph(filt='Austen')
-    markov_tree = generate_markov_tree(mhyph_entries, order=ORDER)
-print "Done generating Markov tree."
 
 available_domains = []
 
+words = generate_words(method='syllables', order=1, number_of_words=100)
+
 while len(available_domains) < DOMAINS_TO_GENERATE:
-    new_sequence = generate_new_sequence(markov_tree, MAX_SEQUENCE_SIZE)
-    if method == PHONES_METHOD:
-        word = phones_to_word(new_sequence)
-    elif method == SYLLABLES_METHOD:
-        word = ''.join(new_sequence)
+    if not words:
+        words = generate_words(method='syllables',
+                               order=1, number_of_words=100)
+    word = words.pop()
     domain = "%s.com" % word
     try:
         # Check the whois to see if the domain is taken
