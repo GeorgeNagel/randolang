bb472acb570c9640d667d40f42c9c4c1318b66b3
gsusmobile <gsnagel@gmail.com>
2014-12-21 12:40:15 -0500

    Cleaning up generate_words script.

--- generate_words.py
+++ generate_words.py
@@ -1,7 +1,21 @@
+import sys
+
 from randolang import generate_words
+from tools.words_cache import WordsCache
 
-
-words = generate_words(method='syllables', order=1, number_of_words=100)
-
-story = ' '.join(words)
-print "%s" % story
+if __name__ == '__main__':
+    number_of_words = int(sys.argv[1])
+    method = sys.argv[2]
+    order = int(sys.argv[3])
+    print "Number of words: %d. Method: %s. Order: %d" % (
+        number_of_words, method, order
+    )
+    print "Generating words..."
+    words_cache = WordsCache()
+    words_cache.load_all_caches()
+    words_cache = generate_words(number_of_words=number_of_words,
+                                 order=order,
+                                 words_cache=words_cache,
+                                 method=method)
+    words_cache.save_all_caches()
+    print "Done"
